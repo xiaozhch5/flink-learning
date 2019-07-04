5d5b08a959573b2efeb17a52b80033ca0321c2c2
xiaozhch5 <xiaozhch5@mail2.sysu.edu.cn>
2019-07-04 17:38:42 +0800

    commit the flink-learning project

---
+++ src/main/java/org/flink/learning/connectors/ReadFromKafka.java
@@ -0,0 +1,30 @@
+package org.flink.learning.connectors;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
+
+/**
+ *@className ReadFromKafka
+ *@description TODO
+ *@author zhchxiao
+ *
+ *@date 19-7-4
+ **/
+ 
+public class ReadFromKafka {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        Properties kafkaProp = new Properties();
+        kafkaProp.put("bootstrap.servers", "hadoop1:9092");
+        kafkaProp.put("group.id", "test");
+
+        DataStream<String> dataStream =  env.addSource(new FlinkKafkaConsumer<>("flinktest", new SimpleStringSchema(), kafkaProp));
+        dataStream.print();
+        env.execute("read from kafka");
+    }
+}
